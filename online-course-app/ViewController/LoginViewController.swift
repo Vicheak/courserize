a0d47a375601021a2929db172a53ea9521529c62
Vicheak <suonvicheak991@gmail.com>
2024-07-28 10:34:09 +0700

    init

--- online-course-app/ViewController/LoginViewController.swift
+++ online-course-app/ViewController/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  online-course-app
 //
 //  Created by @suonvicheakdev on 28/7/24.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
