c0fa623e1d2904cb2b81bdfffb6fd82a52785ac6
Vicheak <suonvicheak991@gmail.com>
2024-08-01 11:45:40 +0700

    update project

---
+++ online-course-app/Util/Alert.swift
@@ -0,0 +1,14 @@
+//
+//  Alert.swift
+//  online-course-app
+//
+//  Created by @suonvicheakdev on 1/8/24.
+//
+
+import Foundation
+
+enum Alert: String {
+    case success = "Success"
+    case warning = "Warning"
+    case cross = "Invalid"
+}
